31aac82ed541801879f057ef37134356ff366815
akarin <i@akarin.info>
2021-07-27 15:03:40 -0400

    VapourSynth/lsmashsource.c, lwlibav_source.c: rename options to ff_options for consistency
    Signed-off-by: akarin <i@akarin.info>

--- VapourSynth/lsmashsource.c
+++ VapourSynth/lsmashsource.c
@@ -105,7 +105,7 @@ VS_EXTERNAL_API(void) VapourSynthPluginInit( VSConfigPlugin config_func, VSRegis
     register_func
     (
         "LWLibavSource",
-        "source:data;stream_index:int:opt;cache:int:opt;cachefile:data:opt;" COMMON_OPTS "repeat:int:opt;dominance:int:opt;ff_loglevel:int:opt;cachedir:data:opt;options:data:opt;",
+        "source:data;stream_index:int:opt;cache:int:opt;cachefile:data:opt;" COMMON_OPTS "repeat:int:opt;dominance:int:opt;ff_loglevel:int:opt;cachedir:data:opt;ff_options:data:opt;",
         vs_lwlibavsource_create,
         NULL,
         plugin
